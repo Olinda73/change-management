b880b4ff6842301fdfa8033138c0fc572835827a
Olinda Cadena <44624345+Olinda73@users.noreply.github.com>
2019-09-23 11:48:30 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,24 +1,25 @@
-<!-- The core Firebase JS SDK is always required and must be listed first -->
-<script src="https://www.gstatic.com/firebasejs/6.6.2/firebase-app.js"></script>
 
-<!-- TODO: Add SDKs for Firebase products that you want to use
-     https://firebase.google.com/docs/web/setup#available-libraries -->
+<script src="https://www.gstatic.com/firebasejs/6.6.2/firebase-app.js">
 
-<script>
-  // Your web app's Firebase configuration
-  var firebaseConfig = {
-    apiKey: "AIzaSyCFx6EEPuadPlNmiWDpXJZmo5jifmakrhk",
-    authDomain: "nbc-changemanagement.firebaseapp.com",
-    databaseURL: "https://nbc-changemanagement.firebaseio.com",
-    projectId: "nbc-changemanagement",
-    storageBucket: "nbc-changemanagement.appspot.com",
-    messagingSenderId: "230090365679",
-    appId: "1:230090365679:web:36c2f650f6b3ab561ed91a"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
 </script>
-// Reference messages collection
+
+firebase.initializeApp(firebaseConfig);
+
+<body></body>
+const firebaseConfig = {
+  apiKey: "AIzaSyCFx6EEPuadPlNmiWDpXJZmo5jifmakrhk",
+  authDomain: "nbc-changemanagement.firebaseapp.com",
+  databaseURL: "https://nbc-changemanagement.firebaseio.com",
+  projectId: "nbc-changemanagement",
+  storageBucket: "nbc-changemanagement.appspot.com",
+  messagingSenderId: "230090365679",
+  appId: "1:230090365679:web:36c2f650f6b3ab561ed91a"
+};
+
+
+
+
+
 var messagesRef = firebase.database().ref('contactformmessages');
 
 $('#contactForm').submit(function(e) {
